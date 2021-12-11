fdbb51437f1459d9db21bc126783ea781ced6062
gazindr <yeenreallly@gmail.com>
2021-12-11 18:39:00 +0500

    Try to start

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// load the things we need
 var express = require('express');
 var app = express();
 
@@ -7,19 +6,9 @@ app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
-// index page 
+// index page
 app.get('/', function(req, res) {
-    var mascots = [
-        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-        { name: 'Tux', organization: "Linux", birth_year: 1996},
-        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-    ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
-
-    res.render('pages/index', {
-        mascots: mascots,
-        tagline: tagline
-    });
+    res.render('pages/index');
 });
 
 // about page
@@ -27,5 +16,5 @@ app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
-app.listen(8088);
-console.log('8088 is the magic port');
+app.listen(8080);
+console.log('Server is listening on port 8080');
